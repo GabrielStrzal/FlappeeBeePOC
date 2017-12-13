3b9e1c161066b7192cfa6df94ca50f33fb3f60a1
Gabriel Strzal <gabrielstrzal@gmail.com>
2017-12-13 11:26:10 -0200

    project config

---
+++ desktop/src/com/bee/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bee.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bee.game.FlapeeBeePOC;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlapeeBeePOC(), config);
+	}
+}
